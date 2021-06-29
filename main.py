dcb1362af22a1245436d7d2156e947780547fc5d
Rahul <rpkumar004@gmail.com>
2021-06-29 22:11:58 +0530

    model and graph

--- main.py
+++ main.py
@@ -3,5 +3,13 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-time_studied = np.array([20,50,32,65,23,43,10,5,22,35,29,5,56])
-scores = np.array([56,83,47,93,47,82,45,78,55,67,57,4,12])
+time_studied = np.array([20,50,32,65,23,43,10,5,22,35,29,5,56]).reshape(-1,1)
+scores = np.array([56,83,47,93,47,82,45,78,55,67,57,4,12]).reshape(-1,1)
+
+model = LinearRegression()
+
+model.fit(time_studied,scores)
+
+plt.scatter(time_studied,scores)
+plt.ylim(0,100)
+plt.show()
